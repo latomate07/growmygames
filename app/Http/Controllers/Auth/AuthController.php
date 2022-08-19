78f4dd7a95798493df1cfe3b50cf1f929d15f4c6
Barry <barrylaouanmagagi@gmail.com>
2022-08-19 20:03:43 +0200

    Redirect logged user if he tries to access some pages

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,10 +7,18 @@ use App\Http\Controllers\Controller;
 class AuthController extends Controller
 {
     public function login() {
+        if(auth()->check()) {
+            return back();
+        }
+
         return view("auth.Login");
     }
 
     public function signin() {
+        if(auth()->check()) {
+            return back();
+        }
+        
         return view("auth.Signin");
     }
 }
