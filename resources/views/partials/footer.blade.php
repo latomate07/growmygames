e0052eb80fb7935b922892e42286a0d3778d0cea
Barry <barrylaouanmagagi@gmail.com>
2022-08-21 23:26:36 +0200

    Delete some section & Edit

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,13 +8,13 @@
         </div>
         <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Informations</h2>
                 <ul class="text-gray-600 dark:text-gray-400">
                     <li class="mb-4">
-                        <a href="#" class="hover:underline">Flowbite</a>
+                        <a href="#" class="hover:underline">Mentions légales</a>
                     </li>
                     <li>
-                        <a href="#" class="hover:underline">Tailwind CSS</a>
+                        <a href="#" class="hover:underline">Confidentialité</a>
                     </li>
                 </ul>
             </div>
