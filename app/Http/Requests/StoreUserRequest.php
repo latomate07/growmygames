2990a55ce74ef845cf8b1b57cbd7a90a7b7a4321
Barry <barrylaouanmagagi@gmail.com>
2022-09-14 20:59:26 +0200

    add unique e-mail

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -25,7 +25,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => 'required|max:20',
-            'email' => 'required',
+            'email' => 'required', 'unique',
             'password' => 'required',
             'role' => 'required',
             'avatar' => 'required|image'
